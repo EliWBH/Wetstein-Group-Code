74f2d25f98595c79934d46672724294f53ed9f96
Eliot Keener <eliotkeener@gmail.com>
2025-04-16 14:25:25 -0500

    Eos and Annie have matching data formats

--- laser_calibration/calibration_runs/run623/eos_data/extract.py
+++ laser_calibration/calibration_runs/run623/eos_data/extract.py
@@ -3,6 +3,7 @@ import sys
 import uproot
 import csv
 import pandas as pd
+import json
 import awkward as ak
 
 # filename = "eos_data-fiber_calibration-5-3-run-623.0.ntuple.root"
@@ -16,19 +17,19 @@ with uproot.open(str(sys.argv[1])) as file:
 
     #Extract the output data
     data = file['output']
-    pre_time = data['digitTime'].array()
 
     #Way to extract the event number (ideally want to find a better way)
     event_number = []
-    counter = 1
-    for event in pre_time:
-        for value in event:
-            event_number.append(counter)
-        counter += 1
+    
+    #Storing the data to CSV inspired by Eli Brunner-Huber
+    branches =  ['digitPMTID', 'digitTime', 'digitCharge']
+    df = data.arrays(branches, library="pd")
 
-    time = ak.flatten(data['digitTime'].array())
-    charge = ak.flatten(data['digitCharge'].array())
-    num_hits = ak.flatten(data['digitPMTID'].array())
+for col in df:
+    df[col] = df[col].apply(json.dumps)
+csv_file_path = "outputData.csv"
+df.to_csv(csv_file_path, index=True)
+
     
 #Make the meta data CSV    
 with open('metaData.csv', 'w', newline='') as meta_csv:
@@ -40,11 +41,3 @@ with open('metaData.csv', 'w', newline='') as meta_csv:
         meta_writer.writerow({'PMT ID': i, 'X Position': x_pos[i], 'Y Position': y_pos[i], 'Z Position': z_pos[i]})
     meta_csv.close()
 
-#Make the output data CSV
-with open('outputData.csv', 'w', newline='') as data_csv:
-    fieldnames = ['Event Number', 'PMT ID', 'Time', 'Charge']
-    data_writer = csv.DictWriter(data_csv, fieldnames=fieldnames)
-    data_writer.writeheader()
-
-    for i in range(0, len(num_hits)):
-        data_writer.writerow({'Event Number': event_number[i], 'PMT ID': num_hits[i], 'Time':time[i], 'Charge':charge[i]})
